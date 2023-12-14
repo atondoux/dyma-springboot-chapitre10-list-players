9431ff1e915132235a83f1049733416e5be0f506
Aurélien <atondoux@gmail.com>
2023-12-14 21:42:57 +0100

    :sparkles: list players

---
+++ src/main/java/com/dyma/tennis/repository/PlayerRepository.java
@@ -0,0 +1,8 @@
+package com.dyma.tennis.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
