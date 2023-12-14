010cfb671cc5d651b339f54266e033b6fa2419a5
Aurélien <atondoux@gmail.com>
2023-12-14 21:34:42 +0100

    :tada: init list players

---
+++ src/main/java/com/dyma/tennis/service/PlayerService.java
@@ -0,0 +1,66 @@
+package com.dyma.tennis.service;
+
+import com.dyma.tennis.Player;
+import com.dyma.tennis.PlayerList;
+import com.dyma.tennis.PlayerToSave;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PlayerService {
+
+    public List<Player> getAllPlayers() {
+        return PlayerList.ALL.stream()
+                .sorted(Comparator.comparing(player -> player.rank().position()))
+                .collect(Collectors.toList());
+    }
+
+    public Player getByLastName(String lastName) {
+        return PlayerList.ALL.stream()
+                .filter(player -> player.lastName().equals(lastName))
+                .findFirst()
+                .orElseThrow(() -> new PlayerNotFoundException(lastName));
+    }
+
+    public Player create(PlayerToSave playerToSave) {
+        return getPlayerNewRanking(PlayerList.ALL, playerToSave);
+    }
+
+    public Player update(PlayerToSave playerToSave) {
+        getByLastName(playerToSave.lastName());
+
+        List<Player> playersWithoutPlayerToUpdate = PlayerList.ALL.stream()
+                .filter(player -> !player.lastName().equals(playerToSave.lastName()))
+                .toList();
+
+        RankingCalculator rankingCalculator = new RankingCalculator(playersWithoutPlayerToUpdate, playerToSave);
+        List<Player> players = rankingCalculator.getNewPlayersRanking();
+
+        return players.stream()
+                .filter(player -> player.lastName().equals(playerToSave.lastName()))
+                .findFirst().get();
+    }
+
+    public void delete(String lastName) {
+        Player playerToDelete = getByLastName(lastName);
+
+        PlayerList.ALL = PlayerList.ALL.stream()
+                .filter(player -> !player.lastName().equals(lastName))
+                .toList();
+
+        RankingCalculator rankingCalculator = new RankingCalculator(PlayerList.ALL);
+        rankingCalculator.getNewPlayersRanking();
+    }
+
+    private Player getPlayerNewRanking(List<Player> existingPlayers, PlayerToSave playerToSave) {
+        RankingCalculator rankingCalculator = new RankingCalculator(existingPlayers, playerToSave);
+        List<Player> players = rankingCalculator.getNewPlayersRanking();
+
+        return players.stream()
+                .filter(player -> player.lastName().equals(playerToSave.lastName()))
+                .findFirst().get();
+    }
+}
